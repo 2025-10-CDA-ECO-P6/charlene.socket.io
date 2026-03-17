836c740a974aae23285b71031bd28c0f5598c760
ChSPN <charlene.scomparin@gmail.com>
2026-03-17 11:12:10 +0100

    feat/Actualize coverage test and readme.md

--- Test/vitest.config.ts
+++ Test/vitest.config.ts
@@ -5,7 +5,16 @@ export default defineConfig({
     globals: true,
     environment: 'node',
     coverage: {
-      provider: 'v8'
+      provider: 'v8',
+      include: ['src/**/*.ts'],
+      exclude: ['src/**/*.test.ts'],
+      reporter: ['text', 'html'],
+      thresholds: {
+        lines: 80,
+        functions: 80,
+        branches: 80,
+        statements: 80,
+      }
     }
   }
 })
